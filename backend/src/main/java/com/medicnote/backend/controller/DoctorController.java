e6af34ee4613df083f60209af130f00292189dfa
Meghana Reddy <reddymeghana844@gmail.com>
2026-03-13 22:30:21 +0530

    completed backend task

--- backend/src/main/java/com/medicnote/backend/controller/DoctorController.java
+++ backend/src/main/java/com/medicnote/backend/controller/DoctorController.java
@@ -1,27 +1,27 @@
 package com.medicnote.backend.controller;
 
+import com.medicnote.backend.entity.Doctor;
+import com.medicnote.backend.service.DoctorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-import com.medicnote.backend.entity.Doctor;
-import com.medicnote.backend.service.DoctorService;
-
 @RestController
 @RequestMapping("/doctors")
+@CrossOrigin(origins = "*")
 public class DoctorController {
 
     @Autowired
     private DoctorService doctorService;
 
-    @PostMapping
-    public Doctor createDoctor(@RequestBody Doctor doctor) {
-        return doctorService.saveDoctor(doctor);
-    }
-
     @GetMapping
     public List<Doctor> getAllDoctors() {
         return doctorService.getAllDoctors();
+    }
+
+    @PostMapping
+    public Doctor saveDoctor(@RequestBody Doctor doctor) {
+        return doctorService.saveDoctor(doctor);
     }
 }
